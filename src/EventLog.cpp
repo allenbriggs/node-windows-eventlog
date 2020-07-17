e1806db67427ff16422cb5157d872d5baae54c9d
Allen Briggs <allen.briggs@cornerstonenw.com>
2020-07-17 18:31:51 -0400

    use L prefix

--- src/EventLog.cpp
+++ src/EventLog.cpp
@@ -125,7 +125,7 @@ namespace {
 
         EventLog(const std::string& source) {
             std::wstring wideSource(source.begin(), source.end());
-            eventLogHandle_ = RegisterEventSource(wideSource.c_str(), "test");
+            eventLogHandle_ = RegisterEventSource(wideSource.c_str(), L"test");
             if (!eventLogHandle_) {
                 Nan::ThrowError(("Unable to register event source: " + getLastErrorAsString()).c_str());
             }
